a0b7423bb026f5546e4bba13b068c565b377d260
shyhaishyh <562314771@qq.com>
2015-10-19 16:34:37 +0800

    11

--- src/cn/wd/HelloWorld.java
+++ src/cn/wd/HelloWorld.java
@@ -8,6 +8,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("Hello World!!");
+			
+			double aaa= 10.8;
+			int bbb = (int) aaa;
+			System.out.println(bbb);
 	}
 
 }
---
+++ src/cn/wd/Three.java
@@ -0,0 +1,27 @@
+package cn.wd;
+
+import java.util.Scanner;
+
+public class Three {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.print("张三的JAVA课成绩是：");
+		double zjava = input.nextDouble();
+		System.out.print("张三的SQL课成绩是：");
+		double zsql = input.nextDouble();
+		System.out.print("张三的数学课成绩是：");
+		double zmath = input.nextDouble();
+		
+		double differ = zjava - zsql;
+		System.out.println("JAVA和SQL课的分数之差为：" + differ);
+		double avg = (zjava + zsql + zmath)/3;
+		System.out.println("3门课的平均分是：" + avg);
+		
+	}
+
+}
